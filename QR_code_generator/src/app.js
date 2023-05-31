bfa64c4c9eb2a27f854de8f377026bc265a5a236
Hansaka Tennakoon <90048384+han-zyx@users.noreply.github.com>
2023-05-31 10:40:51 +0530

    update0.2

--- QR_code_generator/src/app.js
+++ QR_code_generator/src/app.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+const path =  require('path');
+const qrcode = require('qrcode')
 
 const port = process.env.port || 3800 ;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+const viewsPath = path.join(__dirname, '../FrontEnd');
+app.use(express.static("public"));
+
+
+app.set('view engine', 'ejs');
+app.set('views',viewsPath);
+
 app.get("/" , (req,res,next) => {
     res.render('index')
 });
+
+app.post("/scan", (req, res, next) => {
+    const input_text = req.body.text;
+    qrcode.toDataURL(input_text, (err, src) => {
+        if (err) res.send("Something went wrong!!");
+        res.render("scan", {
+            qr_code: src,
+        });
+    });
+});
+
 
 app.listen(port,console.log(`Listening on port ${port}`));
 
